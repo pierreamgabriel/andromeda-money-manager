5ef1c0b3ba725e828a347e23d0760815ad3a081c
Pierre Macedo <pierre.alexander.mg@gmail.com>
2022-03-28 11:20:55 -0300

    multiple changes concerning the database

--- screens/Addincome.js
+++ screens/Addincome.js
@@ -1,150 +1,156 @@
-import React, { useEffect } from 'react';
-import { ScrollView, StatusBar, StyleSheet, SafeAreaView, View, Pressable, Modal } from 'react-native';
-import { Header as HeaderRNE, Input, CheckBox, Overlay, Text, Button, Icon } from 'react-native-elements';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, SafeAreaView, View, Pressable } from 'react-native';
+import { Header as HeaderRNE, Input, CheckBox, Overlay, Text, Icon } from 'react-native-elements';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import { rnrw } from 'react-native-realm-wrapper';
-import {MainButton, OkButton} from '../components/Buttons';
-import {taxRnrw, nonTaxRnrw} from '../Schemas';
+import { MainButton, OkButton } from '../components/Buttons';
+import { taxRnrw, nonTaxRnrw } from '../Schemas';
+import Checkbox from '../components/Checkbox';
 
-function Addincome ({ navigation, route }) {
-	const [description, setDesc] = React.useState("");
-	const [amount, setAm] = React.useState("");
-	const [date, setDate] = React.useState(new Date().toLocaleDateString());
-	const [isDatePickerVisible, setDatePickerVisibility] = React.useState(false);
-	const [paymentDate, setPaymentDate] = React.useState("Select a payment date");
-	const [tax, setTax] = React.useState(false);
-    const [nonTax, setNonTax] = React.useState(false);
-	const [visible, setVisible] = React.useState(false);
-	
-	const showDatePicker = () => {	
+function AddExpense({ navigation, route }) {
+  const [data, setData] = useState({
+    desc: "",
+    amount: "",
+    amount_: "",
+    tax: false,
+    nonTax: false,
+    date: [],
+    paymentDate: "Select a payment date",
+  });
+  const amount = (arg1, arg2) => {
+    if (isNaN(arg1)) {
+      arg1 = "";
+    }
+    const fixAmount = arg1.toString().replace(/[^0-9]/g, "");
+    const number = parseInt(fixAmount);
+    if (arg2 == "blur") {
+      if (isNaN(number)) {
+        setData({ ...data, amount: "", amount_: "" });
+      } else {
+        setData({ ...data, amount: number, amount_: number.toFixed(2) });
+      }
+    } else {
+      setData({ ...data, amount: number, amount_: fixAmount });
+    }
+  }
+  const [isDatePickerVisible, setDatePickerVisibility] = React.useState(false);
+  const [visible, setVisible] = React.useState(false);
+
+  const showDatePicker = () => {
     setDatePickerVisibility(true);
   };
-	const handleConfirm = (date) => {
-    setDate(date.toLocaleDateString());	
+  const handleConfirm = (date) => {
+    setData({ ...data, date: date.toLocaleDateString() });
     hideDatePicker();
   };
-	const hideDatePicker = () => {
+  const hideDatePicker = () => {
     setDatePickerVisibility(false);
   };
-	const toggleOverlay = () => {
+  const toggleOverlay = () => {
     setVisible(!visible);
   };
 
-	const addData = async () => {
-		if (description == "" || amount == "") {
-			alert("Description and amount are required.");
-		} else if (tax == true){	
-		let id; 	
-		await rnrw.nextID(taxRnrw).then(value => {id = value;});
-		rnrw.write(taxRnrw, {id: id, description: description, amount: parseInt(amount), date: date})
-		.then(value => {
-			if (value == "Success") {
-			toggleOverlay();
-			} else {
-			  alert("Something went wrong. Please try again.");
-			  }
-		})	
-		} else if (nonTax == true) {
-		let id; 	
-		await rnrw.nextID(nonTaxRnrw).then(value => {id = value;});
-		rnrw.write(nonTaxRnrw, {id: id, description: description, amount: parseInt(amount), date: date})
-		.then(value => {
-			if (value == "Success") {
-			toggleOverlay();
-			} else {
-			  alert("Something went wrong. Please try again.");
-			  }
-		})		
-		} else {
-			alert("You need to select Taxable or Nontaxable.");
-		}
-	}
-	
-	useEffect(() => {
-	if (date != new Date().toLocaleDateString())	{
-		
-		setPaymentDate(date);
-	}
-}, [date]);
-	
-	return(
-	<>
-	<SafeAreaView> 
-	<HeaderRNE containerStyle={styles.header}
-	statusBarProps={{backgroundColor: '#80bb55'}}	
-	leftComponent={
-		 <Pressable onPress={navigation.goBack}>
-           <Icon type='font-awesome' name="arrow-left" color="white" />
-         </Pressable>
-		}
-	centerComponent={{ text: 'Add new income', style: styles.center }}	
-		/>	
-	</SafeAreaView>	
-	<View>
-	<Input
-        placeholder="Description"
-        leftIcon={{ type: 'font-awesome', name: 'comment' }}
-        onChangeText={value => setDesc(value)}
-        />	
-    <Input
-        placeholder="Amount"
-        leftIcon={{ type: 'font-awesome', name: 'money' }}
-        onChangeText={value => setAm(value)}
+  const addData = async () => {
+    if (data.desc == "" || data.amount == "" || isNaN(data.amount) || typeof data.date != "string") {
+      alert("All fields are required.");
+    } else if (data.tax == true) {
+      let id;
+      await rnrw.nextID(taxRnrw).then(value => { id = value; });
+      rnrw.write(taxRnrw, { id: id, description: data.desc, amount: data.amount, date: data.date, tax: true })
+        .then(value => {
+          if (value == "Success") {
+            toggleOverlay();
+          } else {
+            alert("Something went wrong. Please try again.");
+          }
+        });
+    } else if (data.nonTax == true) {
+      let id;
+      await rnrw.nextID(nonTaxRnrw).then(value => { id = value; });
+      rnrw.write(nonTaxRnrw, { id: id, description: data.desc, amount: data.amount, date: data.date, tax: false })
+        .then(value => {
+          if (value == "Success") {
+            toggleOverlay();
+          } else {
+            alert("Something went wrong. Please try again.");
+          }
+        })
+    } else {
+      alert("You need to select Taxable or Nontaxable.");
+    }
+  };
+
+  useEffect(() => {
+    if (typeof data.date == "string") {
+      setData({ ...data, paymentDate: data.date });
+    }
+  }, [data.date]);
+
+  return (
+    <>
+      <SafeAreaView>
+        <HeaderRNE containerStyle={styles.header}
+          statusBarProps={{ backgroundColor: '#80bb55' }}
+          leftComponent={
+            <Pressable onPress={navigation.goBack}>
+              <Icon type='font-awesome' name="arrow-left" color="white" />
+            </Pressable>
+          }
+          centerComponent={{ text: 'Add new income', style: styles.center }}
         />
-    <View style={{flexDirection:'row',alignItems:'center',justifyContent:'center'}}>
-   <CheckBox
-        center
-        title="Taxable"
-        checkedIcon="dot-circle-o"
-        uncheckedIcon="circle-o"
-        containerStyle={{backgroundColor:'transparent'}}
-        checked={tax}
-        onPress={() => {setTax(true); setNonTax(false)}}
-      />
-   <CheckBox
-        center
-        title="Nontaxable"
-        checkedIcon="dot-circle-o"
-        uncheckedIcon="circle-o"
-        containerStyle={{backgroundColor:'transparent'}}
-        checked={nonTax}
-        onPress={() => {setTax(false); setNonTax(true)}}
-      /> 
-     </View>
-     <Pressable onPress={showDatePicker}>
-     <Input
-        disabled
-        placeholder={paymentDate}
-        leftIcon={{ type: 'font-awesome', name: 'calendar' }}
-        onChangeText={value => setDesc(value)}
-        />	
-     </Pressable>
-     <DateTimePickerModal
-        isVisible={isDatePickerVisible}
-        mode="date"
-        onConfirm={handleConfirm}
-		onCancel={hideDatePicker}
-      />
-	<MainButton text="Add Income" func={addData} />	
-	<Overlay isVisible={visible} onBackdropPress={toggleOverlay}>
-        <Text style={{marginTop: 10}}>Data added successfully.</Text>
-        <OkButton nav={navigation} route={route}/>
-      </Overlay>	
-	</View>	
+      </SafeAreaView>
+      <View>
+        <Input
+          placeholder="Description"
+          leftIcon={{ type: 'font-awesome', name: 'comment' }}
+          onChangeText={value => setData({ ...data, desc: value })}
+        />
+        <Input
+          placeholder="Amount"
+          leftIcon={{ type: 'font-awesome', name: 'money' }}
+          value={data.amount_}
+          onChangeText={value => amount(value)}
+          onBlur={() => amount(data.amount, "blur")}
+        />
+        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', width: '95%' }}>
+          <Checkbox title="Taxable" checked={data.tax} 
+          func={() => setData({ ...data, tax: true, nonTax: false })}/>
+          <Checkbox title="Nontaxable" checked={data.nonTax}
+          func={() => setData({ ...data, tax: false, nonTax: true })}/>
+        </View>
+        <Pressable onPress={showDatePicker}>
+          <Input
+            disabled
+            placeholder={data.paymentDate}
+            leftIcon={{ type: 'font-awesome', name: 'calendar' }}
+          />
+        </Pressable>
+        <DateTimePickerModal
+          isVisible={isDatePickerVisible}
+          mode="date"
+          onConfirm={handleConfirm}
+          onCancel={hideDatePicker}
+        />
+        <MainButton text="Add Income" func={addData} />
+        <Overlay isVisible={visible} onBackdropPress={toggleOverlay}>
+          <Text style={{ marginTop: 10 }}>Data added successfully.</Text>
+          <OkButton nav={navigation} route={route} />
+        </Overlay>
+      </View>
     </>
-	);
+  );
 }
 const styles = StyleSheet.create({
   header: {
     backgroundColor: '#80bb55',
-	paddingBottom: 10
+    paddingBottom: 10
   },
   center: {
     color: 'white',
     fontSize: 20,
     fontWeight: 'bold',
     width: 180
-  },	
+  },
 });
 
-export default Addincome;
+export default AddExpense;
--- screens/Home.js
+++ screens/Home.js
@@ -1,39 +1,106 @@
-import React, { useEffect } from 'react';
-import { ScrollView, StatusBar, StyleSheet, SafeAreaView, View } from 'react-native';
-import { Header as HeaderRNE, Tab, TabView, Button } from 'react-native-elements';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { Numbers, Charts } from '../components/Charts';
-import Realm from 'realm';
-import {taxable, nontaxable, expenses} from '../Schemas';
+import React, { useEffect, createContext, useState } from 'react';
+import { StyleSheet, SafeAreaView, View, ScrollView } from 'react-native';
+import { Header as HeaderRNE, Tab, TabView, ListItem, Icon } from 'react-native-elements';
+import { Charts } from '../components/Charts';
+import { taxRnrw, nonTaxRnrw, expensesRnrw } from '../Schemas';
+import { AddItemButton } from '../components/Buttons';
+import Checkbox from '../components/Checkbox';
+import { rnrw } from 'react-native-realm-wrapper';
+export const UpdateHome = createContext();
 
-function Home ({ navigation, route }) {
-	
-const [taxableIncome, setTax] = React.useState(null);
-const [nontaxableIncome, setNon] = React.useState(null);
-const [expenses_, setEx] = React.useState(null);
-const [index, setIndex] = React.useState(0);	
-const [update, setUpdate] = React.useState(0);
+function Home({ navigation }) {
 
-useEffect(() => {
-Realm.open({
-    schema: [taxable, nontaxable, expenses]
-  }).then(realm => {
-  setTax(realm.objects("taxable").sum("amount"));	
-  setNon(realm.objects("nontaxable").sum("amount")); 
-  setEx(realm.objects("expenses").sum("amount"));	  	  
-});
-setUpdate(0);	
-}, [taxableIncome, nontaxableIncome, expenses_, update]);
-	
-	return(
+  const [taxableIncome, setTax] = useState(null);
+  const [nontaxableIncome, setNon] = useState(null);
+  const [expenses_, setEx] = useState(null);
+  const [index, setIndex] = useState(0);
+  const [update, setUpdate] = useState();
+  const [checkBoxIncome, setCheckBoxIncome] = useState({
+    tax: true,
+    nonTax: false
+  });
+  const [list, setList] = useState([]);
+
+  const format = (value) => {
+    if (value == null) {
+      return
+    }
+    let decimalPart = value.toFixed(2).split('.')[1]
+    return (
+      value
+        .toString()
+        .split('.')[0]
+        .split('')
+        .reverse()
+        .map((number, index, array) => {
+          return (index + 1) % 3 === 0 && array[index + 1]
+            ? `.${number}`
+            : number;
+        })
+        .reverse()
+        .join('')
+      + ',' + decimalPart
+    )
+  };
+
+  const deleteItem = (index) => {
+
+    if (list[index].tax == true) {
+      rnrw.del(taxRnrw, list[index].id);
+      setList(list.filter((item, i) => i !== index));
+    } else {
+      rnrw.del(nonTaxRnrw, list[index].id);
+      setList(list.filter((item, i) => i !== index));
+    }
+  };
+
+  useEffect(() => {
+    
+    const setVariables = async () => {
+      let arr1;
+      let arr2;
+      let arr3;
+      await rnrw.ot(taxRnrw, "date", true).then(obj => { arr1 = JSON.parse(JSON.stringify(obj)); });
+      await rnrw.ot(nonTaxRnrw, "date", true).then(obj => { arr2 = JSON.parse(JSON.stringify(obj)); });
+      await rnrw.ot(expensesRnrw, "date", true).then(obj => { arr3 = JSON.parse(JSON.stringify(obj)); });
+      
+      if (arr1.length > 0) {
+        setTax(arr1.map(item => item.amount).reduce((prev, next) => prev + next));
+      } else {
+        arr1 = [];
+      }
+      if (arr2.length > 0) {
+        setNon(arr2.map(item => item.amount).reduce((prev, next) => prev + next));
+      } else {
+        arr2 = [];
+      }
+      if (arr3.length > 0) {
+        setEx(arr3.map(item => item.amount).reduce((prev, next) => prev + next));
+      } else {
+        arr3 = [];
+      }
+      if (checkBoxIncome.all) {
+        setList([...arr1, ...arr2]);
+      } else if (checkBoxIncome.tax) {
+        setList(arr1);
+      } else {
+        setList(arr2);
+      }  
+
+    };
+    setVariables();
+    
+  }, [update, checkBoxIncome]);
+
+  return (
     <>
-	<SafeAreaView> 
-	<HeaderRNE containerStyle={styles.header}
-	statusBarProps={{backgroundColor: '#80bb55'}}	
-	leftComponent={{text: 'Andromeda Money', style: styles.left}} >
-	</HeaderRNE>	
-	</SafeAreaView>
-	 <Tab
+      <SafeAreaView>
+        <HeaderRNE containerStyle={styles.header}
+          statusBarProps={{ backgroundColor: '#80bb55' }}
+          leftComponent={{ text: 'Andromeda Money', style: styles.left }} >
+        </HeaderRNE>
+      </SafeAreaView>
+      <Tab
         value={index}
         onChange={(e) => setIndex(e)}
         indicatorStyle={{
@@ -42,17 +109,17 @@ setUpdate(0);
         }}
         variant="primary"
       >
-        <Tab.Item containerStyle={{backgroundColor: '#699847'}}
+        <Tab.Item containerStyle={{ backgroundColor: '#699847' }}
           title="Report"
           titleStyle={{ fontSize: 12 }}
           icon={{ name: 'dollar-sign', type: 'font-awesome-5', color: 'white' }}
         />
-        <Tab.Item containerStyle={{backgroundColor: '#699847'}}
+        <Tab.Item containerStyle={{ backgroundColor: '#699847' }}
           title="Income"
           titleStyle={{ fontSize: 12 }}
           icon={{ name: 'hand-holding-usd', type: 'font-awesome-5', color: 'white' }}
         />
-        <Tab.Item containerStyle={{backgroundColor: '#699847'}}
+        <Tab.Item containerStyle={{ backgroundColor: '#699847' }}
           title="Expenses"
           titleStyle={{ fontSize: 12 }}
           icon={{ name: 'file-invoice-dollar', type: 'font-awesome-5', color: 'white' }}
@@ -60,52 +127,64 @@ setUpdate(0);
       </Tab>
 
       <TabView value={index} onChange={setIndex} animationType="spring">
-        <TabView.Item style={{ backgroundColor: '#80bb55', width: '100%' }}>
-         <Charts taxable={taxableIncome} nontaxable={nontaxableIncome} expenses={expenses_} /> 
+        <TabView.Item style={{ backgroundColor: '#80bb55', width: '100%' }}
+          onMoveShouldSetResponder={(e) => e.stopPropagation()}>
+          <Charts taxable={taxableIncome} nontaxable={nontaxableIncome} expenses={expenses_} />
         </TabView.Item>
-        <TabView.Item style={{ backgroundColor: '#80bb55', width: '100%' }}>
+        <TabView.Item style={{ backgroundColor: '#80bb55', width: '100%' }}
+          onMoveShouldSetResponder={(e) => e.stopPropagation()}>
+          <ScrollView>
+            <UpdateHome.Provider value={setUpdate}>
+              <AddItemButton nav={navigation} screen="AddIncome" />
+            </UpdateHome.Provider>
+            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+              <Checkbox title="Taxable" uncheckedColor="white" color="white" checked={checkBoxIncome.tax}
+                func={() => setCheckBoxIncome({ all: false, tax: true, nonTax: false })} />
+              <Checkbox title="Nontaxable" uncheckedColor="white" color="white" checked={checkBoxIncome.nonTax}
+                func={() => setCheckBoxIncome({ all: false, tax: false, nonTax: true })} />
+            </View>
+            {list.map((item, index) => {
+              return (
+                <ListItem key={index} bottomDivider
+                  style={{
+                    marginBottom: 3,
+                    width: "95%",
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                  }}>
+                  <ListItem.Content>
+                    <ListItem.Title>{item.description}</ListItem.Title>
+                    <ListItem.Subtitle>{item.date}</ListItem.Subtitle>
+                    <ListItem.Subtitle>{format(item.amount)}</ListItem.Subtitle>
+                  </ListItem.Content>
+                  <Icon name="delete" onPress={() => deleteItem(index)} />
+                </ListItem>
+              );
+            })}
+          </ScrollView>
+        </TabView.Item>
+        <TabView.Item style={{ backgroundColor: '#80bb55', width: '100%' }}
+          onMoveShouldSetResponder={(e) => e.stopPropagation()}>
           <View>
-			 <Button
-                onPress={() =>
-                  navigation.navigate('Addincome', {onReturn: (arg) => {
-      setUpdate(arg)
-    }})
-       			}
-                title="Add new"
-                buttonStyle={{
-                  backgroundColor: 'rgba(78, 116, 289, 1)',
-                  borderRadius: 3,			 
-                }}
-                containerStyle={{
-                  width: 120,
-                  marginHorizontal: 50,
-                  marginVertical: 10,
-				  marginLeft:'auto',
-		          marginRight:'auto',
-		          left:0,
-		          right:0				
-                }}
-              />
-			</View>
-        </TabView.Item>
-        <TabView.Item style={{ backgroundColor: '#80bb55', width: '100%' }}>
+            <AddItemButton nav={navigation} screen="AddExpense" />
+          </View>
         </TabView.Item>
       </TabView>
-	</>	
-	);
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
   header: {
     backgroundColor: '#80bb55',
-	paddingBottom: 10
+    paddingBottom: 10
   },
   left: {
     color: 'white',
     fontSize: 20,
     fontWeight: 'bold',
     width: 185
-  },	
+  },
 });
 
 export default Home;
