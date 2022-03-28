213eae8b8857be45b5d4d3d4e899d57d5711d81e
Pierre Macedo <pierre.alexander.mg@gmail.com>
2022-03-28 11:11:45 -0300

    moved the add new button to its own component

--- components/Buttons.js
+++ components/Buttons.js
@@ -1,53 +1,86 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button } from 'react-native-elements';
 import Realm from 'realm';
+import { UpdateHome } from '../screens/Home'
 
-function MainButton (props) {
-	return(
-	<Button
-                onPress={props.func}
-                title={props.text}
-                buttonStyle={{
-                  backgroundColor: 'rgba(78, 116, 289, 1)',
-                  borderRadius: 3,			 
-                }}
-                containerStyle={{
-                  width: 120,
-                  marginHorizontal: 50,
-                  marginVertical: 10,
-				  marginLeft:'auto',
-		          marginRight:'auto',
-		          left:0,
-		          right:0				
-                }}
-              />
-	);
+function MainButton(props) {
+  return (
+    <Button
+      onPress={props.func}
+      title={props.text}
+      buttonStyle={{
+        backgroundColor: 'rgba(78, 116, 289, 1)',
+        borderRadius: 3,
+      }}
+      containerStyle={{
+        width: 120,
+        marginHorizontal: 50,
+        marginVertical: 10,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        left: 0,
+        right: 0
+      }}
+    />
+  );
 }
 
-function OkButton (props) {
-	return(
-	<Button
-                onPress={ async () => {
-	            const realm = await Realm.open();
-	            await realm.close();	
-		        props.route.params.onReturn(1); 
-		        props.nav.goBack();
-	}}
-                title="OK"
-                buttonStyle={{
-                  backgroundColor: 'rgba(78, 116, 289, 1)',
-                  borderRadius: 3,			 
-                }}
-                containerStyle={{
-                  width: 50,
-                  marginTop: 15,
-				  marginLeft:'auto',
-		          marginRight:'auto',
-		          left:0,
-		          right:0				
-                }}
-              />
-	);
+function OkButton(props) {
+  return (
+    <Button
+      onPress={async () => {
+        const realm = await Realm.open();
+        await realm.close();
+        props.route.params.onReturn(1);
+        props.nav.goBack();
+      }}
+      title="OK"
+      buttonStyle={{
+        backgroundColor: 'rgba(78, 116, 289, 1)',
+        borderRadius: 3,
+      }}
+      containerStyle={{
+        width: 50,
+        marginTop: 15,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        left: 0,
+        right: 0
+      }}
+    />
+  );
 }
 
-export {MainButton, OkButton};
+function AddItemButton(props) {
+  const setUpdate = useContext(UpdateHome);
+  return (
+    <Button
+      onPress={() =>{
+        props.nav.navigate(props.screen, {
+          onReturn: (arg) => {
+            setUpdate(arg)
+          }
+        });
+        setUpdate(0);
+      }
+      }
+      title="Add New"
+      buttonStyle={{
+        backgroundColor: 'rgba(78, 116, 289, 1)',
+        borderRadius: 3,
+      }}
+      containerStyle={{
+        width: 120,
+        marginHorizontal: 50,
+        marginVertical: 20,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        left: 0,
+        right: 0
+      }}
+    />
+  );
+
+}
+
+export { MainButton, OkButton, AddItemButton };
